3db9c3c1fa0561e7ce8a3728acd964172b0cf91c
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-03-07 20:42:49 +0100

    added tailwind and typescript

---
+++ README.md
@@ -0,0 +1,2 @@
+# [Mondotosz.github.io](https://mondotosz.github.io/)
+This is my github pages repository
---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+import "https://code.jquery.com/jquery-3.6.0.min.js";
+$("#toggleDarkMode").on("click", () => {
+    if (localStorage.theme === 'dark') {
+        localStorage.theme = 'light';
+    }
+    else {
+        localStorage.theme = 'dark';
+    }
+    setTheme();
+});
+function setTheme() {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    }
+    else {
+        document.documentElement.classList.remove('dark');
+    }
+}
+jQuery(() => {
+    setTheme();
+});
