c7ee4f91fc7374f3d7eb2d49c7766f5a192c90c2
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-11-13 17:08:22 +0100

    updated skill levels display

--- js/components.js
+++ js/components.js
@@ -1,5 +1,67 @@
-document.addEventListener("alpine:init",()=>{
+document.addEventListener("alpine:init", () => {
     // Data
+    Alpine.data('dropdown', () => ({
+        open: false,
 
+        trigger: {
+            ['@click']() {
+                this.open = true
+            },
+        },
+
+        dialogue: {
+            ['x-show']() {
+                return this.open
+            },
+            ['@click.outside']() {
+                this.open = false
+            },
+        }
+    }))
+
+    Alpine.data('languages',()=>({
+        languages:[],
+        styles:["bg-blue-400 border-blue-400","bg-green-400 border-green-400","bg-yellow-400 border-yellow-400"],
+        init(){
+            fetch("/data/languages.json")
+            .then(response => {
+                return response.json()
+            })
+            .then(data=>{
+                this.languages = data
+                this.languages.sort((a,b)=>{return b.level - a.level})
+            })
+        }
+    }))
+
+    Alpine.data('tools',()=>({
+        tools:[],
+        styles:["bg-blue-400 border-blue-400","bg-green-400 border-green-400","bg-yellow-400 border-yellow-400"],
+        init(){
+            fetch("/data/tools.json")
+            .then(response => {
+                return response.json()
+            })
+            .then(data=>{
+                this.tools = data
+                this.tools.sort((a,b)=>{return b.level - a.level})
+            })
+        }
+    }))
+
+    Alpine.data('technologies',()=>({
+        technologies:[],
+        styles:["bg-blue-400 border-blue-400","bg-green-400 border-green-400","bg-yellow-400 border-yellow-400"],
+        init(){
+            fetch("/data/technologies.json")
+            .then(response => {
+                return response.json()
+            })
+            .then(data=>{
+                this.technologies = data
+                this.technologies.sort((a,b)=>{return b.level - a.level})
+            })
+        }
+    }))
     // Stores
 })
